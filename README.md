6733ba34fc3f10e7572b982a6bc72e8a76f5791b
Shubham <shubhamgulati91@gmail.com>
2018-07-25 18:28:08 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# spring-boot-2-jpa-spring-data-rest 
+Sample RESTful API Project with Spring Web, Spring Data, JPA, H2 db and Rest Repositories. 
---
+++ src/main/java/com/example/springboot/jpa/spring/data/rest/SpringBoot2JpaSpringDataRestApplication.java
@@ -0,0 +1,12 @@
+package com.example.springboot.jpa.spring.data.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot2JpaSpringDataRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot2JpaSpringDataRestApplication.class, args);
+	}
+}
